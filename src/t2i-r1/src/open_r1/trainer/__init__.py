50928ab12adab6575a2a8e1720f7b223634cd92a
Yihang Chen <yhangchen@cs.ucla.edu>
2025-10-22 00:47:44 -0700

    first commit: unverified

---
+++ src/t2i-r1/src/open_r1/trainer/__init__.py
@@ -0,0 +1,5 @@
+# from .grpo_trainer import JanusT2IR1Trainer
+from .grpo_trainer_ir import JanusT2IR1Trainer
+from .grpo_trainer_ir_ar import ARR1Trainer
+
+__all__ = ["JanusT2IR1Trainer", "ARR1Trainer"]
